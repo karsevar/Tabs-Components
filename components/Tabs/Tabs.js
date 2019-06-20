aa573c19f1319c5007a46caf3658eeb7224654a5
masonkarsevar <masonkarsevar@gmail.com>
2019-06-20 08:17:48 -0700

    WIP: TabLink class. links passing correct data

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,10 +3,12 @@ class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
     // this.element;
+    this.element = element;
     
     // Get the custom data attribute on the Link
     // this.data;
-    
+    this.data = this.element.dataset.tab;
+    console.log(this.data);
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
     
@@ -59,4 +61,10 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+links = document.querySelectorAll('.tabs-link');
+console.log(links);
+
+links.forEach(link => new TabLink(link));
+
+
+
