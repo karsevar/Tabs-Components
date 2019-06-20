e34fa091f490d1d8f9b921a8ef5a1e81fae2cece
masonkarsevar <masonkarsevar@gmail.com>
2019-06-20 11:38:17 -0700

    found the bug in TabLink method select

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -36,7 +36,7 @@ class TabLink {
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
     this.itemElement = document.querySelector(`div.tabs-item[data-tab="${this.data}"]`)
-    console.log(this.itemElement);
+    // console.log(this.itemElement);
     
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
@@ -54,7 +54,10 @@ class TabLink {
   select() {
     // Get all of the elements with the tabs-link class
     // const links;
-    // const links = document.querySelectorAll('tabs-item');
+    const links = document.querySelectorAll('.tabs-item');
+
+    // console.log('click fired', this.itemElement); // Event listener works perfectly.
+    console.log(links.length);
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
