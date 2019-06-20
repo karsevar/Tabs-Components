d500a7e05b3402938502b5f3f33140dada2eefdf
masonkarsevar <masonkarsevar@gmail.com>
2019-06-20 11:45:03 -0700

    Now the tabs work

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -57,15 +57,15 @@ class TabLink {
     const links = document.querySelectorAll('.tabs-item');
 
     // console.log('click fired', this.itemElement); // Event listener works perfectly.
-    console.log(links.length);
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-    // links.forEach(link => link.classList.remove('tabs-link-selected'));
+    links.forEach(link => link.classList.remove('tabs-item-selected'));
+    console.log(links);
 
     // Add a class named "tabs-link-selected" to this link
     // this.element;
-    // this.itemElement.classList.add('tabs-link-selected');
+    this.itemElement.classList.add('tabs-item-selected');
     
     // Call the select method on the item associated with this link
 
