b57baeffb1ba336f9121a0cd890fbe065cf96231
masonkarsevar <masonkarsevar@gmail.com>
2019-06-20 08:43:34 -0700

    trouble shooting TabLink select method

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -32,7 +32,7 @@ class TabLink {
     // Get the custom data attribute on the Link
     // this.data;
     this.data = this.element.dataset.tab;
-    console.log(this.data) // returning back the proper strings (1, 2, 3, 4)
+    // console.log(this.data) // returning back the proper strings (1, 2, 3, 4)
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
     this.itemElement = document.querySelector(`div.tabs-item[data-tab="${this.data}"]`)
@@ -40,22 +40,32 @@ class TabLink {
     
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
+    this.tabItem = new TabItem(this.itemElement);
+    // console.log(this.tabItem) // Output seems to be displaying the proper object configuration.
     
     // Add a click event listener on this instance, calling the select method on click
+    // this.element.addEventListener('click', (event) => {
+    //   this.tabItem.select();
+    // });
 
+    this.element.addEventListener('click', this.select.bind(this));
   };
 
   select() {
     // Get all of the elements with the tabs-link class
     // const links;
+    // const links = document.querySelectorAll('tabs-item');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
+    // links.forEach(link => link.classList.remove('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
     // this.element;
+    // this.itemElement.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
+
 
   }
 }
